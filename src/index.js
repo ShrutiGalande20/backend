b1aec424166e6a7ac329d3e8e1505fff606040ca
Shruti <shrutigalande2@gmail.com>
2024-07-20 20:47:52 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv';
+
+// import 'dotenv.config';
+
+dotenv.config();
+const app = express();
+// const hostname = 'localhost';
+// const port = 4000;
+
+const hostname = process.env.HOSTNAME;
+const port = process.env.PORT ;
+
+app.use(cors())
+
+app.get('/courses', (req, res) => {
+
+    const courses = [
+        {
+            id : 1,
+            courseName : "MERN"
+        },
+        {
+            id : 2,
+            courseName : "MEAN"
+        },
+        {
+            id : 3,
+            courseName : "MERP"
+        }
+    ]
+
+    res.send(courses);
+})
+
+app.listen(port, () => {
+    console.log(`server is running on http://${hostname}:${port}`);
+})
